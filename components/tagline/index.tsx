9f4c161fe7b4a75c0b56604edd60149160d488d2
mrprst <prost.mathias@gmail.com>
2022-09-18 01:04:54 +0200

    greensock anim

--- components/tagline/index.tsx
+++ components/tagline/index.tsx
@@ -1,29 +1,39 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { Box, Code, Text } from '@mantine/core'
+import { useState, useEffect, useRef } from 'react'
 import classes from './Tagline.module.scss'
 import Button from '../button'
+import { gsap } from 'gsap'
 
 type LocaleProps = {
   localeFile: {
-    taglineTitle: string;
-    taglineSubtitle: string;
+    taglineTitle: string
+    taglineSubtitle: string
     taglineButton: string
-  };
+  }
 }
 
-function Tagline({localeFile}: LocaleProps) {
+function Tagline({ localeFile }: LocaleProps) {
+  const boxRef = useRef<HTMLInputElement>(null)
+  const q = gsap.utils.selector(boxRef)
+
+  useEffect(() => {
+    gsap.timeline()
+    .to(q('h1'), { y: -30 })
+    .to(q('.sub'), { y: -30 })
+    .to(q('div'), { y: -30 })
+  }, [])
 
   return (
-      <div className={classes.container}>
-        <h1 className={classes.title} dangerouslySetInnerHTML={ { __html: localeFile.taglineTitle } }></h1>
-        <p className={classes.content}>
-          {localeFile.taglineSubtitle}
-        </p>
-        <div className={classes.button}>
-          <Button title={localeFile.taglineButton} size="lg" />
-        </div>
+    <div ref={boxRef} className={classes.container}>
+      <h1
+        className={classes.title}
+        dangerouslySetInnerHTML={{ __html: localeFile.taglineTitle }}
+      ></h1>
+      <p className={`sub ${classes.content}`}>{localeFile.taglineSubtitle}</p>
+      <div className={classes.button}>
+        <Button title={localeFile.taglineButton} size="lg" />
       </div>
+    </div>
   )
 }
 
