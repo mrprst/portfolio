e43fd6f0f179ff832f0ed8db500009d9ef868cc3
mrprst <prost.mathias@gmail.com>
2022-09-20 23:12:41 +0200

    animation fixed on menu

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import LanguageModal from '../languagemodal'
 import LanguageSwitch from '../languageswitch'
 import { gsap } from 'gsap'
+import { Observer } from 'gsap/dist/Observer'
 import { useRouter } from 'next/router'
 
 type Props = {
@@ -23,14 +24,26 @@ function Menu({ setShowmenu }: Props) {
       const isNarrowScreen = window.matchMedia('(max-width: 600px)')
       isNarrowScreen.matches ? setOpened(false) : setOpened(true)
     }
-    const items = gsap.utils.toArray(`div.${classes.a}`).reverse()
-    const mainTimeline = gsap.timeline()
+    const items = gsap.utils.toArray(`div.${classes.a}`)
+    console.log(items)
+    gsap.timeline().fromTo(items, { opacity: 0 }, { opacity: 1 })
+
+    function clickAnim(index: number) {
+      let tl = gsap.timeline()
+      tl.to(`.sections`, {
+        yPercent: (-100 / 4) * index,
+        duration: 1.4,
+        ease: 'power.out',
+      })
+    }
+
     items.forEach((item: any, index: number) => {
-      let tl = gsap.timeline().fromTo(item, { opacity: 0 }, { opacity: 1 })
-      mainTimeline.add(tl, index * 0.2)
+      Observer.create({
+        target: item,
+        onClick: () => clickAnim(index + 1),
+      })
     })
   }, [])
-  
 
   useEffect(() => {
     switch (locale) {
@@ -48,25 +61,23 @@ function Menu({ setShowmenu }: Props) {
 
   return (
     <div ref={boxRef} className={classes.menu}>
-      <Link href="#about">
-        <div onClick={() => setShowmenu(false)} className={classes.a}>
-          {menulocale[0]}
-        </div>
-      </Link>
-      <Link href="#projects">
-        <div onClick={() => setShowmenu(false)} className={classes.a}>
-          {menulocale[1]}
-        </div>
-      </Link>
-      <Link href="#contact">
-        <div onClick={() => setShowmenu(false)} className={classes.a}>
-          {menulocale[2]}
-        </div>
-      </Link>
+      <div onClick={() => setShowmenu(false)} className={`${classes.a}`}>
+        {menulocale[0]}
+      </div>
+      <div onClick={() => setShowmenu(false)} className={classes.a}>
+        {menulocale[1]}
+      </div>
+      <div onClick={() => setShowmenu(false)} className={classes.a}>
+        {menulocale[2]}
+      </div>
       <Link href={'/mathias-prost-resume.pdf'} locale={false}>
-        <div onClick={() => setShowmenu(false)} className={classes.a}>
+        <a
+          target="_blank"
+          onClick={() => setShowmenu(false)}
+          className={classes.a}
+        >
           {menulocale[3]}
-        </div>
+        </a>
       </Link>
       <div onClick={() => setShowmenu(false)}>
         {opened ? <LanguageModal /> : <LanguageSwitch />}
