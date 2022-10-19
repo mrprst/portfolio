beae06d8f4754e7dea2f0232019dfb6e7dec6a36
mrprst <prost.mathias@gmail.com>
2022-10-19 10:19:47 +0200

    json file

--- public/locales/french.js
+++ public/locales/french.js
@@ -1,42 +1,41 @@
-// eslint-disable-next-line import/prefer-default-export
 export const french = {
-  taglineTitle: 'Bonjour !<br />Je suis Mathias, développeur web fullstack.',
-  taglineSubtitle: 'Vous trouverez ici tout ce que j\'ai fait dans ma vie qui puisse vous intéresser si vous souhaitez qu\'on collabore ensemble.',
-  taglineButton: 'En savoir plus',
-  aboutTitle: 'A propos',
-  aboutText: 'Après plusieurs années passées à gérer des sites e-commerce je suis désormais web developpeur.<br />Dans ma vie passée, j\'ai eu la chance de créer un disquaire en ligne et service de gravure de vinyle, travailler pour un leader de la tech audio française, un gigantesque site e-commerce et une major du disque. Ces années ont fait de moi un spécialiste e-commerce. <br />Actuellement freelance, je suis ouvert à des opportunités contractuelles en Europe, me permettant de travailler en télétravail.',
-  menu: ['à propos', 'projets', 'contact', 'CV'],
-  profilepicCTA: 'CV',
-  contact: 'On se connecte ?',
-  contactText: 'Pour discuter missions freelance, opportunités ou bien juste me poser une question, ça se passe sur ce gros bouton ci-dessous',
-  contactCTA: 'Oui, celui la 🙃',
-  projects: {
+  "taglineTitle": "Bonjour !<br />Je suis Mathias, développeur web fullstack.",
+  "taglineSubtitle": "Vous trouverez ici tout ce que j'ai fait dans ma vie qui puisse vous intéresser si vous souhaitez qu'on collabore ensemble.",
+  "taglineButton": "En savoir plus",
+  "aboutTitle": "A propos",
+  "aboutText": "Après plusieurs années passées à gérer des sites e-commerce je suis désormais web developpeur.<br />Dans ma vie passée, j'ai eu la chance de créer un disquaire en ligne et service de gravure de vinyle, travailler pour un leader de la tech audio française, un gigantesque site e-commerce et une major du disque. Ces années ont fait de moi un spécialiste e-commerce. <br />Actuellement freelance, je suis ouvert à des opportunités contractuelles en Europe, me permettant de travailler en télétravail.",
+  "menu": ["à propos", "projets", "contact", "CV"],
+  "profilepicCTA": "CV",
+  "contact": "On se connecte ?",
+  "contactText": "Pour discuter missions freelance, opportunités ou bien juste me poser une question, ça se passe sur ce gros bouton ci-dessous",
+  "contactCTA": "Oui, celui la 🙃",
+  "projects": {
     0: {
-      title: 'Peps',
-      description:
-        'Peps est un site de recettes de cuisine codé en Next.js et pensé comme un wiki, ou chaque recette est associée à un plat phare, des pays, régions et ingrédients. Le moyen le plus simple de débattre des meilleures variantes du couscous ou du pot-au-feu.',
-      stack: ['react.js', 'next.js', 'prisma'],
-      github: 'https://github.com/SLAF-Team/Peps/',
-      website: 'https://peps.life/',
-      image: 'peps.jpg',
+      "title": "Peps",
+      "description":
+        "Peps est un site de recettes de cuisine codé en Next.js et pensé comme un wiki, ou chaque recette est associée à un plat phare, des pays, régions et ingrédients. Le moyen le plus simple de débattre des meilleures variantes du couscous ou du pot-au-feu.",
+      "stack": ["react.js", "next.js", "prisma"],
+      "github": "https://github.com/SLAF-Team/Peps/",
+      "website": "https://peps.life/",
+      "image": "peps.jpg"
     },
     1: {
-      title: 'Slaf!',
-      description:
-        'Le Syndicat de Lutte Anti Franglais est un dictionnaire collaboratif faussement sérieux (mais qui pique réellement les yeux) de traduction Français/Franglais inspiré du classique Urban Dictionary.',
-      stack: ['ruby', 'rails'],
-      github: 'https://github.com/SLAF-Team/SLAF_Project/',
-      website: 'https://syndicatdelutteantifranglais.club/',
-      image: 'slaf.jpg',
+      "title": "Slaf!",
+      "description":
+        "Le Syndicat de Lutte Anti Franglais est un dictionnaire collaboratif faussement sérieux (mais qui pique réellement les yeux) de traduction Français/Franglais inspiré du classique Urban Dictionary.",
+      "stack": ["ruby", "rails"],
+      "github": "https://github.com/SLAF-Team/SLAF_Project/",
+      "website": "https://syndicatdelutteantifranglais.club/",
+      "image": "slaf.jpg"
     },
     2: {
-      title: 'Records Tshirt Store',
-      description:
-        'Site vitrine de vente de tshirts floqués avec des disques de mon panthéon personnel. Chaque pochette est flocable avec une couleur différente.',
-      stack: ['react.js', 'next.js', 'prisma'],
-      github: 'https://github.com/mrprst/panopli-app/',
-      website: 'https://records-tshirt-store.vercel.app/',
-      image: 'rts.jpeg',
-    },
-  },
-};
+      "title": "Records Tshirt Store",
+      "description":
+        "Site vitrine de vente de tshirts floqués avec des disques de mon panthéon personnel. Chaque pochette est flocable avec une couleur différente.",
+      "stack": ["react.js", "next.js", "prisma"],
+      "github": "https://github.com/mrprst/panopli-app/",
+      "website": "https://records-tshirt-store.vercel.app/",
+      "image": "rts.jpeg"
+    }
+  }
+}
